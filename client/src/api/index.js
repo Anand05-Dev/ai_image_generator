51a7c31cb4ced3d8be8ac062c42bda9e8462816f
Anand Maharia <anandmaharia2005@gmail.com>
2025-07-12 13:58:37 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
-// http://localhost:8080/api/
 const API = axios.create({
-  baseURL: "https://ai-image-generator-ooe7.onrender.com/api/post",
+  baseURL: "https://ai-image-generator-ooe7.onrender.com/api/",
 });
 
 export const GetPosts = async () => await API.get("/post/");
 export const CreatePost = async (data) => await API.post("/post/", data);
 export const GenerateImageFromPrompt = async (data) =>
-await API.post("/generateImage/", data);
+  await API.post("/generateImage/", data);
